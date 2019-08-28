b7900630a6c008c86b54b58aada2f156e8c5a897
Tylar Pierson <tylarpiersonbusiness@gmail.com>
2019-08-28 15:16:40 -0400

    migrated businesses to schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_27_213626) do
+ActiveRecord::Schema.define(version: 2019_08_28_170217) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "businesses", force: :cascade do |t|
+    t.string "name"
+    t.string "address"
+    t.string "photo"
+    t.string "phone"
+    t.string "coords"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
