b18f27fbafbd2e6c618993b2fb0627c950ae4f6d
Tylar Pierson <tylarpiersonbusiness@gmail.com>
2019-08-28 15:27:54 -0400

    seeded the faker business

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,6 @@ require 'faker'
 
 users = (1..100).collect {{username: Faker::Internet.username, photo: Faker::Avatar.image ,email: Faker::Internet.email}}
 
-businesses = (1..100).collect {{name: Faker::Resturant.name, photo: Faker::Avatar.image, type: "Bar", description: Faker::Resturant.description, review: Faker::Resturant.review}}
+businesses = (1..100).collect {{name: Faker::Restaurant.name, photo: Faker::Avatar.image, type: "Bar", description: Faker::Restaurant.description, review: Faker::Restaurant.review}}
 
 User.create!(users)
