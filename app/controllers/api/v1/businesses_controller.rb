38ac28d992b86490b906b32fb9ef0c8bf0766c01
Tylar Pierson <tylarpiersonbusiness@gmail.com>
2019-08-28 15:21:38 -0400

    added controller boilerplate

---
+++ app/controllers/api/v1/businesses_controller.rb
@@ -0,0 +1,22 @@
+module Api
+    module V1
+        class BusinessesController < ApplicationController
+            def index
+            end
+
+            def show
+            end
+            
+            def create
+            end
+
+            def update
+            end
+
+            def destroy
+            end
+
+            private 
+        end
+    end
+end
