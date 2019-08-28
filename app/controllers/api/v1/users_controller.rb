056b65b3e3079c7ed31a6ec00b04df3c70079fb5
Cesar Napoleon Mejia Leiva <cmejia@gmail.com>
2019-08-28 14:07:31 -0700

    adds CUD methods to UserController

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,48 +1,63 @@
 module Api
   module V1
     class UsersController < ApplicationController
-     def index
+      def index
         @users = User.all
         @formatted_users = @users.map { |user| format_user(user) }
 
         render json: @formatted_users
-     end
+      end
 
-     def show
-      @user = User.find(params[:id])
+      def show
+        @user = User.find(params[:id])
 
-      render json: @user
-    end
+        render json: format_user(@user)
+      end
 
-     def create
-     end
-     
-     def update
-     end
-     
-     def destroy
-     end
+      def create
+        @user = User.new(user_params)
 
-     private
-     # Use callbacks to share common setup or constraints between actions.
-     def set_user
-       @user = User.find(params[:id])
-     end
- 
-     # Never trust parameters from the scary internet, only allow the white list through.
-     def user_params
-       params.require(:user).permit(:id, :username, :email, :photo)
-     end
+        if @user.save
+          render json: @user, status: :created
+        else
+          render json: @user.errors, status: :unprocessable_entity 
+        end
+      end
 
-     def format_user(user)
-      {
-        id: user.id,
-        email: user.email,
-        photo: user.photo,
-        username: user.username
-      }
-    end
-     
+      def update
+        @user = User.find(params[:id])
+
+        if @user.update(user_params)
+          render json: @user, status: :created
+        else
+          render json: @user.errors, status: :unprocessable_entity
+        end
+      end
+
+      def destroy
+        User.find(params[:id]).destroy
+        head :no_content
+      end
+
+      private
+        # Use callbacks to share common setup or constraints between actions.
+        def set_user
+          @user = User.find(params[:id])
+        end
+
+        # Never trust parameters from the scary internet, only allow the white list through.
+        def user_params
+          params.require(:user).permit(:id, :username, :email, :photo)
+        end
+
+        def format_user(user)
+          {
+            id: user.id,
+            email: user.email,
+            photo: user.photo,
+            username: user.username
+          }
+        end
     end
   end
 end
