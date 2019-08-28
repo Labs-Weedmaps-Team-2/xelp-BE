a5ff2c3674722f77f34dbdc1b54003073bc2fe20
Tylar Pierson <tylarpiersonbusiness@gmail.com>
2019-08-28 13:04:38 -0400

    created business migration

---
+++ db/migrate/20190828170217_create_businesses.rb
@@ -0,0 +1,13 @@
+class CreateBusinesses < ActiveRecord::Migration[5.2]
+  def change
+    create_table :businesses do |t|
+      t.string :name
+      t.string :address
+      t;string :photo
+      t.string :phone
+      t.string :coords
+
+      t.timestamps
+    end
+  end
+end
