1207e494b714d2b60067e6f0b4dfa74fb3e0eb79
luis <mrluismartinezzz@gmail.com>
2019-08-29 16:29:43 -0700

    def a method that returns a username

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,13 +1,13 @@
 class SessionsController < ApplicationController
   def create
-            # raise request.env["omniauth.auth"].to_yaml
-            auth = request.env["omniauth.auth"]
+    # raise request.env["omniauth.auth"].to_yaml
+    auth = request.env["omniauth.auth"]
 
-            user = User.find_by_provider_and_uid(auth["provider"], 
-            auth["uid"]) || User.create_with_omniauth(auth)
+    user = User.find_by_provider_and_uid(auth["provider"], 
+    auth["uid"]) || User.create_with_omniauth(auth)
 
-            session[:user_id] = user.id
-            
-            render json: user
+    session[:user_id] = user.id
+
+    render json: user
   end
 end
