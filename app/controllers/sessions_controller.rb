6fd428ceb2b9a3c4358f9f642ff3e738e2ae83a2
luis <mrluismartinezzz@gmail.com>
2019-08-29 22:12:31 -0700

    SessionController create method redirects to frontEnd

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,13 +1,16 @@
 class SessionsController < ApplicationController
   def create
-            # raise request.env["omniauth.auth"].to_yaml
-            auth = request.env["omniauth.auth"]
+    # raise request.env["omniauth.auth"].to_yaml
+    auth = request.env["omniauth.auth"]
 
-            user = User.find_by_provider_and_uid(auth["provider"], 
-            auth["uid"]) || User.create_with_omniauth(auth)
+    user = User.find_by_provider_and_uid(auth["provider"], 
+    auth["uid"]) || User.create_with_omniauth(auth)
 
-            session[:user_id] = user.id
-            
-            render json: user
+    session[:user_id] = user.id
+    if ENV['RAILS_ENV'] == "development"
+      redirect_to "http://localhost:4000"
+    else
+      redirect_to "https://pensive-mclean-75bb36.netlify.com"
+    end
   end
 end
