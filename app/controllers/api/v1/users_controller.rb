eaa482e23f1a39ed7c8c5161fb071bae7b1c262f
Cesar Napoleon Mejia Leiva <cmejia@gmail.com>
2019-09-01 10:54:25 -0700

    fixes before_action typo

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,7 +1,7 @@
 module Api
   module V1
     class UsersController < ApplicationController
-      before_action :set_user, only: [:show :update :destroy]
+      before_action :set_user, only: [:show, :update, :destroy]
 
       def index
         @users = User.all
