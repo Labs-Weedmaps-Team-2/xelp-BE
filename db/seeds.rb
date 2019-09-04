2e47c72d192e399d6468364a4b8239a9909bd4c5
Holly Giang <holly.giang@gmail.com>
2019-09-04 11:12:12 -0400

    Add Faker comments

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,9 @@
 require 'faker'
-
+User.delete_all
+Business.delete_all
 users = (1..100).collect {{username: Faker::Internet.username, photo: Faker::Avatar.image ,email: Faker::Internet.email}}
 
+comments = (1..100).collect {{ame: Faker::Commerce.product_name: Faker::Company.catch_phrase}}
+
 User.create!(users)
+Comment.create!(comments)
