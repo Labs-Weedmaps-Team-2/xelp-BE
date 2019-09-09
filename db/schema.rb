71fd0b01f1fd8bc3178b8d5abfe6f9394479f7fe
luis <mrluismartinezzz@gmail.com>
2019-09-09 13:17:20 -0700

    schema update on reflection to adding phone att

--- db/schema.rb
+++ db/schema.rb
@@ -50,6 +50,7 @@ ActiveRecord::Schema.define(version: 2019_09_03_021939) do
     t.string "price"
     t.text "hours"
     t.string "category"
+    t.string "phone"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
