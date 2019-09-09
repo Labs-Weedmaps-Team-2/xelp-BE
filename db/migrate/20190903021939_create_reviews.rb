9a4933cdfec38d27168676f419a4e2b4529f3332
luis <mrluismartinezzz@gmail.com>
2019-09-09 13:00:49 -0700

    timestamps place on business & review tables

--- db/migrate/20190903021939_create_reviews.rb
+++ db/migrate/20190903021939_create_reviews.rb
@@ -4,7 +4,8 @@ class CreateReviews < ActiveRecord::Migration[5.2]
       t.references :user, foreign_key: true
       t.references :business, foreign_key: true
       t.text :text
-      t.float :rating
+      t.integer :rating
+      t.timestamps
     end
   end
 end
