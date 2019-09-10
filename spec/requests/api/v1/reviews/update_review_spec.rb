0d7e1cbd516138a8076d05671d8f117d261bf4d4
Tylar Pierson <tylarpiersonbusiness@gmail.com>
2019-09-10 09:56:30 -0400

    added update tests

--- spec/requests/api/v1/reviews/update_review_spec.rb
+++ spec/requests/api/v1/reviews/update_review_spec.rb
@@ -2,8 +2,18 @@ require 'rails_helper'
 
 RSpec.describe 'Update Review' type: :request do
   describe 'Update /api/v1/business/:id/review/:id' do
+    let(:review_params) { { review: { text: 'this place is active !!', id: 1} } }
+    let(:review_params_update) { { review: { text: "this place isn't active", id: 1} } }
+    let(:business) {double('Business', name: 'Awesome_Snake_Bar', id: 1, yelp_id:) }
+
     it 'updates a review on a business' do
-      put '/api/v1/business/:id/review/:id'
+      put '/api/v1/business/:id/review/:id', params: review_params_update 
+      json_body = JSON.pars(response.body).deep_symbolize_keys
+
+      expect(response).to have_http_status(201)
+      expect(json_body).to include({
+        text: "this place isn't active"
+      })
     end
   end
 end
