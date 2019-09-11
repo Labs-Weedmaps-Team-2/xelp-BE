d19489e1d9a44f8b8e309e5f5533ede5eae1f4c2
luis <mrluismartinezzz@gmail.com>
2019-09-11 16:59:47 -0700

    reverted to old create method on review controller

--- app/controllers/api/v1/review_controller.rb
+++ app/controllers/api/v1/review_controller.rb
@@ -26,26 +26,13 @@ module Api
       end
 
       def create
-        @business = Business.find_by(yelp_id: params[:id]) || self.create_bus(params[:id])
-        # @review = Review.new(review: "tessttttting")
-        # Review.create_from_review(@review)
-        # render json: @review
+        @business = Business.find_by(yelp_id: params[:id]) || Business.create!(yelp_id: params[:id])
 
-        # current 
-        @review = Review.new(text: params[:value], business_id: @business.id, user_id: session[:user_id])
+        @review = Review.new(text: params[:review][:text], rating: params[:review][:rating], user_id: session[:user_id], business_id: @business.id)
         if @review.save
           render json: format_review_json(@review), status: :created
-        @business = Business.find_by(yelp_id: params[:id]) || Business.create!(yelp_id: params[:id])
-        if Review.reviewable(@business.id, session[:user_id])
-          @review = Review.new(text: params[:review][:text], rating: params[:review][:rating], user_id: session[:user_id], business_id: @business.id)
-          if @review.save
-            render json: format_review_json(@review), status: :created
-          else
-            render json: @review.errors, status: :unprocessable_entity 
-          end
         else
-          @review = Review.find_by(user_id: session[:user_id], business_id: @business.id)
-          render json: format_review_json(@review)
+          render json: @review.errors, status: :unprocessable_entity 
         end
       end
 
