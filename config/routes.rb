bf29c618d93f1727773ff9f682b8376311985e22
luis <mrluismartinezzz@gmail.com>
2019-09-11 14:49:20 -0700

    redirect on logout route

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'welcome#index'
   get "/auth/:provider/callback" => "sessions#create"
-  get "/logout" => "sessions#destroy"
+  get "/auth/logout" => "sessions#destroy"
   namespace :api do
     namespace :v1 do
       get "/users/current_user" => "users#current_user"
