13150818693f7bd8dfb74446f3b4f51d481cdb72
Brad Mortensen <b.mort86@gmail.com>
2019-09-10 22:13:13 -0600

    current user action test

--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -9,7 +9,16 @@ RSpec.describe Api::V1::UsersController, type: :controller do
       expect(response.status).to eq(200)
       expect(response.content_type).to eq("application/json")
       expect(response).to have_http_status(:success)
-      puts :formatted_users
+    end
+  end
+  describe "GET current_user" do
+    it "should return a user in db" do
+  # It Should return a list of all users in DB 
+      get :current_user 
+      expect(response.status).to eq(200)
+      expect(response.content_type).to eq("application/json")
+      expect(response).to have_http_status(:success)
+      expect(response).to include("party")
     end
   end
 end
