19368475d75fe5be33d2f5ff91c52d2f873d63e1
Cesar Napoleon Mejia Leiva <cmejia@gmail.com>
2019-09-20 01:36:42 -0700

    adds category to search query

--- app/models/business.rb
+++ app/models/business.rb
@@ -2,6 +2,6 @@ class Business < ApplicationRecord
   has_many_attached :photos
   has_one_attached :image_url
   has_many :reviews, dependent: :destroy
-  scope :geo, -> (minLat, maxLat, minLng, maxLng, term) {  where("latitude >= :minLat AND latitude <= :maxLat AND longitude >= :minLng AND longitude <= :maxLng AND lower(name) LIKE :term ", {minLat: minLat, maxLat: maxLat, minLng: minLng, maxLng: maxLng, term: "%#{term.downcase}%"} )}
+  scope :geo, -> (minLat, maxLat, minLng, maxLng, term) {  where("latitude >= :minLat AND latitude <= :maxLat AND longitude >= :minLng AND longitude <= :maxLng AND (lower(name) LIKE :term OR lower(category) LIKE :term ) ", {minLat: minLat, maxLat: maxLat, minLng: minLng, maxLng: maxLng, term: "%#{term.downcase}%"} )}
 
 end
